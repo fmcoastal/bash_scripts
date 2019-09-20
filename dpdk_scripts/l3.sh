4b3e0119a0e3b212c0bced344d42718d51ceda4e
Frank Smith <fsmith@cavium.com>
2019-09-20 16:31:08 -0400

    modified:   l3.sh add traffic gen commands to get traffic to pass

--- dpdk_scripts/l3.sh
+++ dpdk_scripts/l3.sh
@@ -2,3 +2,17 @@
 
 /home/misc/dpdk-l3fwd  -c 0x800000 -n 4 -- -p 0x3 --config="(0, 0, 23),(1, 0, 23)"
 
+
+default XAUI20
+src.ip  101.0.0.0
+src.port  101
+dest.ip  100.10.0.1
+dest.port  11
+
+default XAUI30
+dest.ip  101.0.0.0
+dest.port  101
+src.ip  100.10.0.1
+src.port  11
+
+
